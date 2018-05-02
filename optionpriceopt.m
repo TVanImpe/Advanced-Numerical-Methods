9f12454b32367da3f87f056bea33dc3af375a87e
SabineWilke <37119994+SabineWilke@users.noreply.github.com>
2018-05-02 12:50:37 +0200

    Update optionpriceopt.m

--- optionpriceopt.m
+++ optionpriceopt.m
@@ -9,11 +9,12 @@ clc
 
 %% Input
 S=50:10:150; % a vector of option prices, can be varied
+%probably also strike price, time to maturity, price of underlying etc.?
 
 %% Optimization
 
 %{
-we need: parameters that need to be determined
+we need: parameters that need to be determined --> mu, sigma, lambda and all that stuff?
          find out whether we need more input, e.g. a density/
             characteristic function
          the optimization algorithm --> find out how differential evolution
